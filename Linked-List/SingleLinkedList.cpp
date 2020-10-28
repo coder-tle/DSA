14fe8e3b47bb35e01770d6a04aeda0db25fbd707
Sarfaraz Alam <sarfarazalam.sa53@gmail.com>
2020-10-28 15:46:38 +0530

    Added insertion at end, at beggining, after the node operation for singly linked list

--- Linked-List/SingleLinkedList.cpp
+++ Linked-List/SingleLinkedList.cpp
@@ -16,11 +16,17 @@ void Display(struct node * start);
 void Count(struct node * start);
 void Search(struct node * start, int data);
 
+// Insertion in Singly Linked List
+
+struct node * AddAtBeg(struct node *start, int data);
+struct node * AddAtEnd(struct node *start, int data);
+struct node * AddAfter(struct node *start, int data, int item);
+// struct node * AddBefore(struct node * start, int data);
 
 int main()
 {
 	struct node * start = NULL;
-	int choice, data;
+	int choice, data, item;
 	
 	while(1)
 	{
@@ -28,7 +34,12 @@ int main()
 		cout<<"2. Display List\n";
 	    cout<<"3. Count nodes of List\n";
 		cout<<"4. Search\n";
-		cout<<"5. Quit\n";
+		cout<<"5. Add to empty list / Add at beginning\n";
+		cout<<"6. Add at end\n";
+		cout<<"7. Add after node\n";
+		cout<<"8. Add before node\n";
+		
+		cout<<"9. Quit\n";
 		
 		cout<<"Enter your choice : ";
 		cin>>choice;
@@ -50,6 +61,30 @@ int main()
 			Search(start, data);
 			break;
 		case 5:
+			cout<<"Enter the element to be inserted : ";
+			cin>>data;
+			start = AddAtBeg(start, data);
+			break;
+		case 6:
+			cout<<"Enter the element to be inserted : ";
+			cin>>data;
+			start = AddAtEnd(start, data);
+			break;
+		case 7 :
+			cout<<"Enter the element to be inserted : ";
+			cin>>data;
+			cout<<"Enter the element after which to insert : ";
+			cin>>item;
+			start = AddAfter(start, data, item);
+			break;
+		case 8 :
+			cout<<"Enter the element to be inserted : ";
+			cin>>data;
+			//start = AddBefore(start, data);
+			break;
+			
+			
+		case 9:
 			exit(1);
 		default :
 			cout<<"Wrong choice\n";
@@ -144,6 +179,86 @@ void Search(struct node * start, int data)
 }
 
 
+struct node * AddAtBeg(struct node *start, int data)
+{
+	struct node * temp;
+	if(start == NULL)
+	{
+		temp = new node;
+		temp->data = data;
+		temp->link = NULL;
+		start = temp;
+		return start;
+	}
+	
+	temp = new node;
+	temp->data = data;
+	temp->link = start;
+	start = temp;
+	return start;
+	
+}
 
+struct node * AddAtEnd(struct node * start, int data)
+{
+	struct node * temp ;
+	temp = start;
+	
+	if(start == NULL)
+	{
+		temp = new node;
+		temp->data = data;
+		temp->link = NULL;
+		start = temp;
+		return start;
+	}
+	
+	while(temp->link != NULL)
+	{
+		temp = temp->link;
+			
+	}
+	
+	temp->link = new node;
+	temp->link->data = data;
+	temp->link->link = NULL;
+	return start;
+	
+	
+}
 
-
+struct node * AddAfter(struct node * start, int data, int item)
+{
+	struct node * temp;
+	temp = start;
+	
+	
+	// This is doing add before
+	//while( ( temp->link != NULL ) && (temp->link->data != item) )
+	//{
+		//temp = temp->link;
+	//}
+	
+	// Simply search the node which has value == item
+	struct node * p;
+	while(temp != NULL)
+	{
+		if(temp->data == item)
+		{
+				p = new node;
+				p->data = data;
+				
+				p->link = temp->link;
+				temp->link = p;
+				// break;
+				return start;
+		}
+		temp = temp->link;
+	}
+	//p = new node;
+	//p->data = data;
+	//p->link = temp->link;
+	//temp->link = p;
+	cout<<item<<" is not present in the list\n";
+	return start;
+}
