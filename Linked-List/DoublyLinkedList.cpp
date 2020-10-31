bd72bec9d0339599f6472970e2255e940f9aba90
Sarfaraz Alam <sarfarazalam.sa53@gmail.com>
2020-10-31 17:36:16 +0530

    Added insert operation at beginning, at end, to empty list and creation of doubly linked list

--- Linked-List/DoublyLinkedList.cpp
+++ Linked-List/DoublyLinkedList.cpp
@@ -10,6 +10,19 @@ struct node{
 	struct node * next;
 };
 
+// Create Doubly Linked List
+struct node * CreateList(struct node * start);
+
+// Display 
+void Display(struct node * start);
+
+
+// Insertion function 
+struct node * AddToEmptyList(struct node * start, int data);
+struct node * AddAtBeginning(struct node * start, int data);
+struct node * AddAtEnd(struct node * start, int data);
+
+
 int main()
 {
 	int choice, data, item;
@@ -19,6 +32,10 @@ int main()
 	{
 		cout<<"1. Create List\n";
 		cout<<"2. Display\n";
+		cout<<"3. Add to empty list\n";
+		cout<<"4. Add at beginning\n";
+		cout<<"5. Add at end\n";
+		
 		cout<<"10. Quit\n";
 		
 		cout<<"Enter your choice : ";
@@ -28,11 +45,29 @@ int main()
 	switch(choice)
 	{
 		case 1:
-		break;
+			start = CreateList(start);
+			break;
 		
-		case 2: 
-		break;
+		case 2:
+			Display(start);
+			break;
 		
+		case 3:
+			cout<<"Enter the element to be inserted : ";
+			cin>>data;
+			start = AddToEmptyList(start, data);
+			break;
+		case 4:
+			cout<<"Enter the element to be inserted : ";
+			cin>>data;
+			start = AddAtBeginning(start, data);
+			break;
+		case 5:
+			cout<<"Enter the element to be inserted : ";
+			cin>>data;
+			start = AddAtEnd(start, data);
+			break;
+			
 		case 10:
 		exit(1);//Whit
 		
@@ -43,3 +78,96 @@ int main()
 	
 	} // End of while loop
 }
+
+void Display(struct node * start)
+{
+	struct node * temp = start;
+	
+	if(temp == NULL)
+	{
+		cout<<"List is empty\n";
+		return ;
+	}
+	
+	while(temp != NULL)
+	{
+		cout<<temp->info<<" ";
+		temp = temp->next;
+	}
+	cout<<endl;
+	return;
+	
+}
+
+
+
+struct node * AddToEmptyList(struct node * start, int data)
+{
+	struct node * temp = new node;
+	temp->info = data;
+	temp->next = temp->prev = NULL;
+	start = temp;
+	return start;
+}
+
+
+struct node * AddAtBeginning(struct node * start, int data)
+{
+	struct node * temp = new node;
+	temp->info = data;
+	
+	temp->next = start;
+	temp->prev = NULL;
+	start = temp;
+	
+	return start;
+	
+	
+}
+
+struct node * AddAtEnd(struct node * start, int data)
+{
+	struct node * temp, *p;
+	temp = new node;
+	temp->info = data;
+	
+	p = start;
+	
+	while(p->next != NULL)
+	{
+		p = p->next;
+	}
+	
+	p->next = temp;
+	temp->prev = p;
+	temp->next = NULL;
+	
+	return start;
+	
+	
+}
+struct node * CreateList(struct node * start)
+{
+	struct node * temp;
+	start = NULL;
+	int n, data;
+	
+	cout<<"Enter number of nodes of the list : ";
+	cin>>n;
+
+	cout<<"Enter the value of nodes : ";
+	cin>>data;
+	
+	start  = AddToEmptyList(start, data);
+	
+	for(int i = 2; i<=n; i++)
+	{
+		cin>>data;
+		
+		start = AddAtEnd(start, data);
+	}
+	return start;
+}
+
+
+
