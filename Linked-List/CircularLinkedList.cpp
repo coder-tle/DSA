b8dac08a49512793d9f2b6ce460f63d9a17296f4
Sarfaraz Alam <sarfarazalam.sa53@gmail.com>
2020-11-06 23:57:09 +0530

    Added template code for circular linked list

---
+++ Linked-List/CircularLinkedList.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+	int info;
+	struct node* link;
+}
+
+int main()
+{
+		int choice, data, item;
+		struct node * last = NULL;
+		
+		while(1)
+		{
+			cout<<"1. Create List\n";
+			cout<<"2. Display\n";
+			cout<<"3. Add to empty list\n";
+			cout<<"4. Add at beginning\n";
+			cout<<"5. Add at end\n";
+			cout<<"6. Add after\n";
+			cout<<"7. Delete\n";
+			cout<<"8. Quit\n";
+			
+			cout<<"Enter your choice : ";
+			cin>>choice;
+			
+			
+			switch(choice)
+			{
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break
+				case 6: 
+					break;
+				case 7:
+					break;
+				case 8:
+					break;
+				
+				default:
+					cout<<"Wrong choice\n";
+			}
+		}
+					
+					
+			
+}
